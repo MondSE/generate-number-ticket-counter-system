6c4d067552fe822c76693cb0223575545e7f1847
Almond Tuazon <azirtuazon@gmail.com>
2025-06-29 21:51:56 +0800

    update next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/numbering-system",
+  assetPrefix: "/numbering-system/",
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
